1b614e3f4fa372f6ab8835b570362935c05027a1
Kevin J. Sung <kevjsung@umich.edu>
2026-06-16 08:28:30 -0400

    overflow visible

--- src/HexGrid.js
+++ src/HexGrid.js
@@ -100,6 +100,7 @@ export default function HexGrid(props) {
           left: 0,
           width: "100%",
           height: "100%",
+          overflow: "visible",
           zIndex: 0,
         },
         viewBox: `0 0 ${totalWidth} ${totalHeight}`,
